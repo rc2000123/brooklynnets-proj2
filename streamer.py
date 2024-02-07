200e824906dc387a476c8012395ca214bb91b9e0
Benjamin Zitzewitz <bzu7626@moore.wot.eecs.northwestern.edu>
2024-02-07 12:15:25 -0600

    finish part 3 A

--- streamer.py
+++ streamer.py
@@ -3,6 +3,7 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 import struct
+import concurrent.futures
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -15,20 +16,41 @@ class Streamer:
         self.dst_port = dst_port
         self.seq_num = 0
         self.expect_recieve = 0
+        self.closed = False
         
         #a dict of the seq number and byte value
         self.buffer = {}
-        
+        self.receive_buffer = []
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        executor.submit(self.listener)
+
+    def listener(self):
+        while not self.closed:
+            try:
+                data, addr = self.socket.recvfrom()
+                if data != b'':
+                    unpacked_value = struct.unpack('i', data[:4])
+
+                    # The rest is the list of bytes
+                    byte_list_unpacked = data[4:]
+                    
+                    print(unpacked_value[0],byte_list_unpacked)
+                    self.buffer[unpacked_value[0]] = byte_list_unpacked
+                
+            except Exception as e:
+                print("listener died!")
+                print(e)
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         # Your code goes here!  The code below should be changed!
         # for now I'm just sending the raw application-level data in one UDP payload
         segmented_bytes = []
-        while (len(data_bytes) > 1472 - 4):
-            new_segment = data_bytes[:1472 - 4]
+        header_size = 4
+        while (len(data_bytes) > 1472 - header_size):
+            new_segment = data_bytes[:1472 - header_size]
             segmented_bytes.append(new_segment)
-            data_bytes = data_bytes[1472 - 4:]
+            data_bytes = data_bytes[1472 - header_size:]
         
         if len(data_bytes) > 0:
             segmented_bytes.append(data_bytes)
@@ -42,20 +64,31 @@ class Streamer:
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
+    
         
         # this sample code just calls the recvfrom method on the LossySocket
         
-        while self.expect_recieve not in self.buffer:
-            data, addr = self.socket.recvfrom()
-            
-            unpacked_value = struct.unpack('i', data[:4])
-
-            # The rest is the list of bytes
-            byte_list_unpacked = data[4:]
-            
-            print(unpacked_value[0],byte_list_unpacked)
-            self.buffer[unpacked_value[0]] = byte_list_unpacked
+        #wait until buffer populates
+        #while len(self.receive_buffer) == 0:
+        #    continue
         
+
+        #wait until buffer populates
+        while self.expect_recieve not in self.buffer:
+            continue
+        
+        
+        #while self.expect_recieve not in self.buffer:
+        #data = self.buffer[self.expect_recieve]
+        #unpacked_value = struct.unpack('i', data[:4])
+
+        # The rest is the list of bytes
+        #byte_list_unpacked = data[4:]
+        
+        #print(unpacked_value[0],byte_list_unpacked)
+        #self.buffer[unpacked_value[0]] = byte_list_unpacked
+    
+    
         # For now, I'll just pass the full UDP payload to the app
         
         byte_array = self.buffer.pop(self.expect_recieve)
@@ -66,4 +99,5 @@ class Streamer:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
-        pass
+        self.closed = True
+        self.socket.stoprecv()
